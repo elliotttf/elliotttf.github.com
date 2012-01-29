72461ab07f8f862dc6625f41dc7240c569ce580a
Elliott Foster <elliottf@codebrews.com>
2012-01-29 11:41:15 -0600

    Dynamic menu and graceful fallback if some bozo doesn't have JS enabled.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var MENU_ITEMS = 4;
+
 $(document).ready(function() {
   // Build the projects menu from GitHub so I don't have to update
   // what I've been working on.
@@ -6,20 +8,21 @@ $(document).ready(function() {
     getGitHubMenu(false);
   }
   else {
-    //menu = localStorage.getItem('menu');
+    menu = localStorage.getItem('menu');
     if (typeof menu === 'undefined' || menu === null) {
       getGitHubMenu(true);
     }
     else {
+      menu = JSON.parse(menu);
       setMenu(menu);
     }
   }
 
-  $('.menu-content').hide();
   $('.menu-toggle').click(function() {
     var parentMenu = $(this).parents('.menu');
     $(parentMenu).toggleClass('menu-active');
     $('.menu-content', parentMenu).fadeToggle('fast');
+    return false;
   });
 });
 
@@ -31,10 +34,35 @@ $(document).ready(function() {
  */
 function getGitHubMenu(store) {
   $.getJSON(
-    'https://api.github.com/users/elliotttf/repos',
+    'https://api.github.com/users/elliotttf/repos?callback=?',
     function onGet(res) {
+      var menu = [];
+      $.each(res.data, function eachRepo(k, repo) {
+        if (repo.name === 'elliotttf.github.com') {
+          return;
+        }
+        var d = new Date(repo.pushed_at);
+        var menuItem = {
+          'href': repo.html_url,
+          'title': repo.name,
+          'date': d.getTime()
+        };
+        menu.push(menuItem);
+      });
+
+      menu.sort(function compare(left, right) {
+        if (left.date < right.date) {
+          return -1;
+        }
+        else if (left.date > right.date) {
+          return 1;
+        }
+        return 0;
+      });
+      menu.reverse();
+
       if (store) {
-        localStorage.setItem('menu', res);
+        localStorage.setItem('menu', JSON.stringify(menu));
       }
       setMenu(menu);
     }
@@ -48,7 +76,7 @@ function getGitHubMenu(store) {
  *   Array of menu items to add to the projects menu.
  */
 function setMenu(menu) {
-  for (var x in menu) {
+  for (var x = 0; x < menu.length && x < MENU_ITEMS; x++) {
     $('.menu-content .menu-items').append(
       '<li class="item"><a href="' + menu[x].href + '">' + menu[x].title + '</a></li>'
     );
